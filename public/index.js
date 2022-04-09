cb19953046bd2a2bbd9b118ec17a8d1ff4e7ff01
Shil rj <shiljr@gmail.com>
2022-04-09 18:15:55 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,20 +1,21 @@
-const UserContext = React.createContext(null);
-
 function Spa() {
   return (
-    <HashRouter>
-      <NavBar/>
-      <UserContext.Provider value={{users:[{name:'peter',email:'peter@mit.edu',password:'secret',balance:100}]}}>
-        <div className="container" style={{padding: "20px"}}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/balance/" component={Balance} />
-          <Route path="/alldata/" component={AllData} />
-        </div>
-      </UserContext.Provider>      
+     <HashRouter>
+      <div>
+        <NavBar/>        
+        <UserContext.Provider value={{users:[{name:'Peter',email:'peter@mit.edu',password:'secret',balance:1000}]}}>
+          <div className="container" style={{padding: "20px"}}>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/login/" component={Login} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            {/* <Route path="/transactions/" component={Transactions} /> */}
+            <Route path="/balance/" component={Balance} />
+            <Route path="/alldata/" component={AllData} />
+          </div>
+        </UserContext.Provider>
+      </div>
     </HashRouter>
   );
 }
