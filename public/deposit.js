fe14b3366faa4759946e7e9db34d1104a2a50a5e
Shil rj <shiljr@gmail.com>
2022-04-23 16:37:38 -0400

    Add files via upload
    Code changes for negative value and Nan changes

--- public/deposit.js
+++ public/deposit.js
@@ -1,42 +1,63 @@
 function Deposit(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState(''); 
+  const [deposit, setDeposit]       = React.useState('');
+  const [balance, setBalance] = React.useState('');
+  const ctx = React.useContext(UserContext);  
+
+  function validate(field, label){
+      if (!field) {
+        setStatus('Error: ' + label);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
+      return true;
+  }
+
+  function checkNumber(field, label) {
+    if (isNaN(field)) {
+      setStatus('Alert: not a number : ' + label + 'failed, Deposit again');
+    if (field < 0) 
+        setStatus('Alert: negative number: ' + label + 'failed, Deposit again');
+      return false;
+    }
+    return true;
+  }
+
+  function handleDeposit(){
+    console.log(deposit);
+    if (!validate(deposit,    'deposit'))    return;
+    if (!checkNumber(deposit,    'deposit'))    return;
+    setBalance(balance);
+    ctx.users.push({deposit,Balance:100});
+    setShow(false);
+  }    
+
+  function clearForm(){
+    setName('');
+    setDeposit('');
+    setPassword('');
+    setShow(true);
+  }
 
   return (
     <Card
       bgcolor="primary"
       header="Deposit"
       status={status}
-      body={show ? 
-        <DepositForm setShow={setShow} setStatus={setStatus}/> :
-        <DepositMsg setShow={setShow} setStatus={setStatus}/>}
+      body={show ? (  
+              <>
+              Balance   <input type="input" value={balance} onChange={e => setBalance(deposit + balance)}/><br/>
+              Deposit Amount<br/>
+              <input type="input" className="form-control" id="deposit" placeholder="Enter deposit" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}/><br/>
+              <button type="submit" className="btn btn-light" onClick={handleDeposit}>Deposit</button>
+              </>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Deposit Received</button>
+              </>
+            )}
     />
   )
 }
-
-function DepositForm(props){    
-    const [deposit, setDeposit] = React.useState('');
-  
-    function handle(){
-      fetch(`/account/update/${deposit}`)
-      .then(response => response.text())
-      .then(text => {
-          try {
-              const data = JSON.parse(text);
-              props.setStatus(JSON.stringify(data.value));
-              props.setShow(false);
-              console.log('JSON:', data);
-          } catch(err) {
-              props.setStatus('Deposit Success')
-              console.log('err:', text);
-          }
-      });
-    }
-  
-    return(
-      <>     
-      Deposit Amount<br/>
-      <input type="number" className="form-control" placeholder="Deposit Amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}/><br/>  
-      <button disabled={!deposit} type="submit" className="btn btn-light" onClick={handle}>Deposit</button>  
-    </>);
-  }
